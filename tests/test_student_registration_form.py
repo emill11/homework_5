ba5ee7cfe49a8a81ca0ff00e71f485291fe3d437
Emil <emil88337@gmail.com>
2024-02-05 15:28:29 +0300

    after homework review

--- tests/test_student_registration_form.py
+++ tests/test_student_registration_form.py
@@ -1,76 +1,59 @@
-from selene import browser, have, be, by
-import time
-
+import pytest
+from selene.support.shared import browser
+from selene import have
+from selene import command
+import os
 
 
 def test_student_registration_form():
 
-    browser.config.window_width = 2000
-    browser.config.window_height = 1000
+    browser.config.window_width = 1000
+    browser.config.window_height = 2000
+    browser.open('/automation-practice-form')
 
-    browser.open('https://demoqa.com/automation-practice-form')
-
+    #WHEN
     browser.element('#firstName').type('Ivan')
     browser.element('#lastName').type('Ivanov')
-
     browser.element('#userEmail').type('test@test.test')
-
-    #пол
-    browser.element('.custom-control-label').click()
-
+    browser.all('[name=gender]').element_by(have.value('Male')).element('..').click()
     browser.element('#userNumber').type('1234567890')
 
-    #поле дата рождения
     browser.element('#dateOfBirthInput').click()
-
-    #выбор года
     browser.element('.react-datepicker__year-select').click()
     browser.element('option[value="2000"]').click()
-
-    #выбор месяца
     browser.element('.react-datepicker__month-select').click()
     browser.element('option[value="3"]').click()
-
-    #выбор дня
     browser.element('div[aria-label="Choose Monday, April 3rd, 2000"]').click()
 
-    #предмет
-    browser.element('#subjectsInput').type('maths').press_enter()
+    browser.element('#subjectsInput').type('ma').press_enter()
+    browser.element('label[for="hobbies-checkbox-1"]').perform(command.js.scroll_into_view).click()
 
-    #чекбокс
-    browser.element('label[for="hobbies-checkbox-1"]').click()
-
-    #адрес
-    browser.element('#currentAddress').type('Street 123')
-
-    #штат
-    browser.element('#react-select-3-input').type('NCR').press_enter()
-
-    #город
-    browser.element('#react-select-4-input').type('Delhi').press_enter()
-
-    #файл
     browser.element('label[for="uploadPicture"]').click()
-    browser.element('#uploadPicture').send_keys('C:/file_test/photo-44.jpg')
+    import tests
+    (browser.element('#uploadPicture').set_value(
+        os.path.abspath(
+        os.path.join(os.path.dirname(tests.__file__), 'resources/photo.jpg')
+    )))
 
-    browser.element('#submit').click()
-    time.sleep(20)
+    browser.element('#currentAddress').type('Street 123')
+    browser.element('#react-select-3-input').type('NC').press_enter()
+    browser.element('#react-select-4-input').type('De').press_enter()
 
+    browser.element('#submit').press_enter()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   #THEN
+    browser.element('.table').all('td').even.should(
+        have.texts(
+    'Ivan Ivanov',
+    'test@test.test',
+    'Male',
+    '1234567890',
+    '03 April,2000',
+    'Maths',
+    'Sports',
+    'photo.jpg',
+    'Street 123',
+    'NCR Delhi'))
 
 
 
