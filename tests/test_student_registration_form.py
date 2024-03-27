5bf5b38479377af23db1633925350149bec64e75
Emil <emil88337@gmail.com>
2024-03-27 11:17:03 +0300

    edited for selenoid

--- tests/test_student_registration_form.py
+++ tests/test_student_registration_form.py
@@ -1,9 +1,11 @@
 from data.users import user_1
 from models.pages.registration_page import RegistrationPage
+import allure
 
 registration_page = RegistrationPage()
 
 
+@allure.title("Successful fill form")
 def test_student_registration_form():
     registration_page.open()
     registration_page.register(user_1)
