ca6dbce215f8679884d64a0017cd396e404cf1cb
Emil <emil88337@gmail.com>
2024-03-29 12:18:14 +0300

    add env

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,15 @@
 import pytest
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import browser
 from utils import attach
+import os
+from dotenv import load_dotenv
 
-
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+@pytest.fixture(scope='function')
 def setup_browser(request):
     browser.config.window_width = 1000
     browser.config.window_height = 2000
@@ -20,8 +23,13 @@ def setup_browser(request):
         }
     }
     options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+    selenoid_url = os.getenv('SELENOID_URL')
+
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@{selenoid_url}",
         options=options
     )
 
@@ -32,5 +40,4 @@ def setup_browser(request):
     attach.add_html(browser)
     attach.add_logs(browser)
     attach.add_video(browser)
-
     browser.quit()
